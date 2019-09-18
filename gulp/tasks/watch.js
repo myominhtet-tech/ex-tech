4f734f9e10d4b3c11bb8b7dbff86464769b47121
myomminhtet-tech <myominhtet.mmc@gmail.com>
2019-09-18 10:52:38 +0630

    Completed large hero block and intro content block.

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+
+        }
+    });
+    watch('./app/index.html', function() {
+        browserSync.reload();
+
+    });
+
+    watch('./app/assets/styles/**/*.css', gulp.series('cssInject'));
+
+
+});
+
+gulp.task('cssInject', gulp.series('styles', function() {
+
+    //  gulp.watch("")
+    return gulp.src('./app/temp/styles/style.css')
+        .pipe(browserSync.stream());
+}));
---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+
+
+// gulp.task('default', function() {
+//     console.log("hey you create a task");
+// });
+
+// gulp.task('html', function() {
+//     console.log("imagine something useful being done to your html");
+// });
